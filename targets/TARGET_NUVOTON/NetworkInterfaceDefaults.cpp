0a6c92f4061d12913ba712a725916ff9d38d402d
Chun-Chieh Li <ccli8@nuvoton.com>
2019-07-16 15:36:09 +0800

    Support Nuvoton targets
    Based on mbed-os-pelion-example draft/mbed-bootloader 4.0.2/mbed-cloud-client 3.3.0,
    support Nuvoton targets:
    1.  Support Nuvoton targets:
        (1) NUMAKER_PFM_NUC472
        (2) NUMAKER_PFM_M487
        (3) NUMAKER_IOT_M487 V1.3
        (4) NUMAKER_PFM_M2351
    2.  Replace sd-driver (SD card in SPI bus mode) with COMMONENT_NUSD.lib
        (SD card in SD bus mode) for storage
    3.  Support M2351
        (1) Partition memory
            - ROM: 64KiB/448KiB
            - RAM: 8KiB/88KiB
        (2) Pre-build secure lib/exec and place them in:
            TARGET_NUVOTON/TARGET_NUMAKER_PFM_M2351
        (3) Disable pre-built secure lib/exec to use the custom one above (.mbedignore)
        (4) Force-include custom secure library above into git (.gitignore)
        (5) Build by running:
            $ mbed compile -m NUMAKER_PFM_M2351 -t ARMC6
    4.  With RoT, bootloader and client application must match:
        (1) Insecure (fake) RoT:
            Bootloader/mbed_lib.json: mbed-bootloader.use-kvstore-rot == 0
            mbed-cloud-client/mbed_lib.json: mbed-cloud-client.external-sst-support == 0
        (2) KVStore RoT:
            Bootloader/mbed_lib.json: mbed-bootloader.use-kvstore-rot == 1
            mbed-cloud-client/mbed_lib.json: mbed-cloud-client.external-sst-support == 1
    5.  Change default BlockDevice to NUSD:
        (1) Add component NUSD.
        (2) Override BlockDevice::get_default_instance (SystemStorage.cpp).
            Required since mbed-cloud-client 2.1.0 (arm_uc_blockdevice_ext)
    6.  Enable kvstore with blockdevice type being "other".
        (1) Override get_other_blockdevice() by providing NUSD for "other" blockdevice
            (kv_config.cpp).
        (2) Configure storage type to "FILESYSTEM".
        (3) Configure file system type to "LITTLE".
        (4) Configure block device type to "other".
        (5) Configure external size to 64MiB, which cannot overlap with update-client.
            storage-address/update-client.storage-size.
    7.  Change back UARTSerial tx/rx buffer size from 1024/1024 to 256/256. This can reduce
        memory footprint by (1024 - 256) * 2 = 1.5KiB. Because this configuration influences
        both ESP8266 and default console (platfrom/mbed_retarget.cpp), memory reduction can
        achieve 1.5KiB x 2 = 3KiB.
    8.  Add heap/stack statistics (mbed_app.json/main.cpp).
    9.  Update to mbed-os 5.13.1 (or afterwards) to fix 'undefined pal_plat_osEntropyInject(...)' error.

---
+++ targets/TARGET_NUVOTON/NetworkInterfaceDefaults.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2019, Nuvoton Technology Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mbed.h"
+#include "ESP8266Interface.h"
+
+#define ESP8266_AT_ONBOARD      1   // On-board ESP8266
+#define ESP8266_AT_EXTERN       2   // External ESP8266
+
+#ifndef ESP8266_AT_SEL
+#warning "ESP8266_AT_SEL missing. Defaults to ESP8266_AT_ONBOARD."
+#define ESP8266_AT_SEL          ESP8266_AT_ONBOARD
+#endif
+
+WiFiInterface *WiFiInterface::get_target_default_instance()
+{
+    /* ESP8266Interface(TX, RX, debug, RTS, CTS, RST); */
+
+#if ESP8266_AT_SEL == ESP8266_AT_ONBOARD
+
+    #if TARGET_NUMAKER_IOT_M487
+    static ESP8266Interface esp(PH_8, PH_9, false, A2, A3, PH_3);
+
+    #elif TARGET_NUMAKER_PFM_M2351
+    static DigitalOut esp_pwr_off(PD_7, 1);     // Disable power to on-board ESP8266
+    static ESP8266Interface esp(PD_1, PD_0, false, PD_3, PD_2, NC);
+
+    if ((int) esp_pwr_off) {                    // Turn on on-board ESP8266
+        wait_ms(50);
+        esp_pwr_off = 0;
+        wait_ms(50);
+    }
+
+    #else
+    static ESP8266Interface esp;
+
+    #endif
+
+#elif ESP8266_AT_SEL == ESP8266_AT_EXTERN
+
+    #if TARGET_NUMAKER_PFM_NANO130
+    static ESP8266Interface esp(D1, D0, false, PB_6, PB_7, D2);
+
+    #elif TARGET_NUMAKER_PFM_NUC472
+    static ESP8266Interface esp(PD_15, PF_0, false, PD_14, PD_13, D2);
+
+    #elif TARGET_NUMAKER_PFM_M453
+    static ESP8266Interface esp(A3, A2, false, A5, A4, D2);
+
+    #elif TARGET_NUMAKER_PFM_M487 || TARGET_NUMAKER_IOT_M487
+    static ESP8266Interface esp(D1, D0, false, A2, A3, D2);
+
+    #elif TARGET_NUMAKER_PFM_M2351
+    static ESP8266Interface esp(D1, D0, false, A3, A2, D2);
+
+    #else
+    static ESP8266Interface esp;
+
+    #endif
+
+#endif
+
+    return &esp;
+}
